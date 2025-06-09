c0f23875610a5c9f4a83031070b1b09f389b0e17
zehata <zehata@gmail.com>
2025-06-09 23:32:03 +0800

    Linted

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -317,7 +317,6 @@ export const Editor = ({
           <div className="editor-preview border-2 whitespace-pre-wrap">
             <Markdown components={{ img: customImage }}>
               {articlePreview.content}
-              
             </Markdown>
           </div>
         </div>
